ed8127e96ab19b51d0987d0318374c500c9d717e
nickwill24 <nick2428.nw@gmail.com>
2021-03-01 20:51:40 -0500

    get route down

--- routes/WorkoutRoute.js
+++ routes/WorkoutRoute.js
@@ -1,6 +1,13 @@
 const Router = require('express').Router()
 const controller = require('../controllers/workout')
 
+
+Router.get('/balanceexercise', controller.balanceExercise )
+Router.get('/strengthexercise', controller.strengthExercise )
+Router.get('/enduranceexercise', controller.enduranceExercise )
+
+
+
 Router.post('/balanceexercise', controller.balanceExercise )
 Router.post('/strengthexercise', controller.strengthExercise )
 Router.post('/enduranceexercise', controller.enduranceExercise )
