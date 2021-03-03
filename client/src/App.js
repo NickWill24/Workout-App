811af6a62c8624e617bddf9e1e6eb9e8a7b745af
nickwill24 <nick2428.nw@gmail.com>
2021-03-03 00:42:28 -0500

    App.js Route Done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Workout from './pages/Workout'
+import Balance from './pages/Balance'
+import Endurance from './pages/Endurance'
+import Strength from './pages/Strength'
 
 class App extends Component {
   render(){
@@ -13,6 +16,9 @@ class App extends Component {
             
             <Route exact path="/" component={Home}/>
             <Route path="/workout" component={Workout}/>
+            <Route path="/balance" component={Balance}/>
+            <Route path="/endurance" component={Endurance}/>
+            <Route path="/Strength" component={Strength}/>
           </Switch>
         </main>
       </div>
