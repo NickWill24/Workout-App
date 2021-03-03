e9290da85c28327b805038621bec57baa41ddb95
nickwill24 <nick2428.nw@gmail.com>
2021-03-02 21:01:26 -0500

    home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,19 @@
 import './styles/App.css'
 import React, { Component } from 'react';
+
+
+export default class App extends Component {
+    constructor() {
+    super()
+    this.state = {
+        
+    }
+    }
+
+    render() {
+    return (
+        <div className="App">
+        </div>
+    )
+    }
+}
