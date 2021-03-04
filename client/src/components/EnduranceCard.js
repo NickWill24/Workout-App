96f2e18f43ad859633100cb68788f92bf96938da
nickwill24 <nick2428.nw@gmail.com>
2021-03-03 23:37:10 -0500

    endurance  page

---
+++ client/src/components/EnduranceCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import '../styles/Card.css'
+
+export default class EnduranceCard extends Component {
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+    const{ name, description, image, time, numOfRep}= this.props
+        return(
+            <div className="card">
+                <p>{name}</p>
+                <p>{description}</p>
+                <p>{numOfRep}</p>
+                <p>{time}</p>
+            </div>
+        )
+        }
+    }
--- client/src/pages/Endurance.js
+++ client/src/pages/Endurance.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import EnduranceCard from '../components/EnduranceCard'
 
 export default class Endurance extends Component {
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
     render(){
+        console.log(this.props.balances)
+        if(this.props){
         return(
             <div>
-                <h1>Endurance</h1>
+                <h1>Balance</h1>
+                {this.props.endurances.map((endurance, index)=>(
+                    <EnduranceCard 
+                        key={index} 
+                        image={endurance.image}
+                        name={endurance.name}
+                        numOfRep={endurance.numOfRep}
+                        time={endurance.time}
+                        description={endurance.description}
+                    
+                    
+                    />
+                ))}
             </div>
-            )
+            )}
+            return <h1>Loading</h1>
         }
     }
