8596456aed25746c986795ccc63bab90c7841f29
nickwill24 <nick2428.nw@gmail.com>
2021-03-03 20:21:25 -0500

    ins style to home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
-
+import "../styles/Home.css"
 export default class Home extends Component {
     render(){
         return(
             <div className="home">
                 <h1>Home</h1>
-                <p>Pick A Catergory</p>
-                <div>
-                    Balance
-                </div>
-                <div>
-                    Endurance
-                </div>
-                <div>
-                    Strength
+                <p>Pick A Category</p>
+                <div className="row">
+                    <div className="category">
+                        Balance
+                    </div>
+                    <div className="category">
+                        Endurance
+                    </div>
+                    <div className="category">
+                        Strength
+                    </div>
                 </div>
             </div>
         )
